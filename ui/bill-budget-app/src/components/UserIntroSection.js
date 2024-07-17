9ee7a7d5b6f1035768b128f35db8b1f30b350b45
= <tylerherwig@gmail.com>
2024-07-17 16:32:12 -0400

    Made user intro section dynamic based on context data...

--- ui/bill-budget-app/src/components/UserIntroSection.js
+++ ui/bill-budget-app/src/components/UserIntroSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Box, Typography, Grid, Paper } from '@mui/material';
 import { styled } from '@mui/system';
 import { Line } from 'react-chartjs-2';
@@ -6,6 +6,10 @@ import 'chart.js/auto';
 import Alert from '@mui/material/Alert';
 import { AccountBalance, Payments, QueryStats } from '@mui/icons-material';
 import Divider from '@mui/material/Divider';
+import { PaychecksContext } from './PaychecksContext';
+import { BillsContext } from './BillsContext';
+import { NumericFormat } from 'react-number-format';
+import moment from 'moment';
 
 const InfoPaper = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(3),
@@ -33,25 +37,90 @@ const BillsBackgroundBox = styled(BackgroundBox)(({ theme }) => ({
     marginBottom: theme.spacing(2)
 }));
 
-const chartData = {
-    labels: ['Jun', 'Jul', 'Aug', 'Sep'],
-    datasets: [
-        {
-            label: 'Income',
-            data: [2630, 2630, 2630, 2630],
-            borderColor: 'rgba(76, 175, 80, 1)',
-            backgroundColor: 'rgba(76, 175, 80, 0.2)'
-        },
-        {
-            label: 'Expenses',
-            data: [2201.82, 3375.46, 1165.26, 1165.26],
-            borderColor: 'rgba(54, 162, 235, 1)',
-            backgroundColor: 'rgba(54, 162, 235, 0.2)'
-        },
-    ],
-};
-
 const UserIntroSection = () => {
+    const { paychecks } = useContext(PaychecksContext);
+    const { bills } = useContext(BillsContext);
+
+    const [upcomingData, setUpcomingData] = useState(null);
+    const [chartData, setChartData] = useState({ labels: [], paychecks: [], bills: [] });
+
+    useEffect(() => {
+        const currentDate = new Date();
+
+        const upcomingPaycheck = paychecks.find(paycheck => new Date(paycheck.date) > currentDate);
+        setUpcomingData(upcomingPaycheck);
+
+        const labelsSet = new Set();
+        const paycheckTotalByMonth = {};
+        const billsChartData = [];
+
+        paychecks.forEach(paycheck => {
+            let paycheckMonth = moment.utc(paycheck.date).format('YYYY-MM');
+            labelsSet.add(moment.utc(paycheckMonth).format('MMM') + ', ' + moment.utc(paycheckMonth).format('YYYY'));
+
+            if (!paycheckTotalByMonth[paycheckMonth]) {
+                paycheckTotalByMonth[paycheckMonth] = 0;
+            }
+            paycheckTotalByMonth[paycheckMonth] += paycheck.amount;
+        });
+
+        const paychecksChartData = Object.entries(paycheckTotalByMonth)
+            .sort((a, b) => new Date(a[0]) - new Date(b[0]))
+            .map(entry => entry[1]);
+
+        const labels = Array.from(labelsSet);
+
+        bills.forEach(billYears => {
+            let totalBillsByMonth = billYears.months.map(month => month.totalAmount);
+            billsChartData.push(...totalBillsByMonth);
+        });
+
+        setChartData({
+            labels: labels,
+            paychecks: paychecksChartData,
+            bills: billsChartData
+        });
+    }, [paychecks, bills]);
+
+    if (!upcomingData) {
+        return null;
+    }
+
+    const handleStatusInfo = (moneyRemaining) => {
+        if (moneyRemaining > 0) {
+            return (
+                <Alert variant="outlined" severity="success">
+                    Great news! You have <b>{<NumericFormat value={moneyRemaining.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} />}</b> remaining after all bills.
+                </Alert>
+            )
+        } else {
+            moneyRemaining = Math.abs(moneyRemaining);
+            return (
+                <Alert variant="outlined" severity="error">
+                    Uh oh! It looks like your bills exceed your paycheck by <b>{<NumericFormat value={moneyRemaining.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} />}</b>.
+                </Alert>
+            )
+        }
+    }
+
+    const dynamicChartData = {
+        labels: chartData.labels,
+        datasets: [
+            {
+                label: 'Paychecks',
+                data: chartData.paychecks,
+                borderColor: 'rgba(76, 175, 80, 1)',
+                backgroundColor: 'rgba(76, 175, 80, 0.2)'
+            },
+            {
+                label: 'Bills',
+                data: chartData.bills,
+                borderColor: 'rgba(54, 162, 235, 1)',
+                backgroundColor: 'rgba(54, 162, 235, 0.2)'
+            },
+        ],
+    };
+
     return (
         <Grid container spacing={2} style={{ marginBottom: '15px' }}>
             <Grid item xs={12} md={6}>
@@ -66,29 +135,27 @@ const UserIntroSection = () => {
                             <PaychecksBackgroundBox>
                                 <Typography variant="h6" style={{fontWeight: 'bold'}}><AccountBalance /> Upcoming Paycheck</Typography>
                             </PaychecksBackgroundBox>
-                            <Typography variant="h3">$526.00</Typography>
-                            <Typography variant="body2">Next payment on July 23rd</Typography>
+                            <Typography variant="h3">{<NumericFormat value={upcomingData.amount.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} />}</Typography>
+                            <Typography variant="body2">Next payment on {moment.utc(upcomingData.date).format('MMMM Do')}</Typography>
                         </Grid>
                         <Grid item xs={6}>
                             <BillsBackgroundBox>
                                 <Typography variant="h6" style={{fontWeight: 'bold'}}><Payments /> Upcoming Bills</Typography>
                             </BillsBackgroundBox>
-                            <Typography variant="h3">$372.58</Typography>
-                            <Typography variant="body2">Prior to July 23rd</Typography>
+                            <Typography variant="h3">{<NumericFormat value={upcomingData.total_bills.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} />}</Typography>
+                            <Typography variant="body2">Prior to {moment.utc(upcomingData.date).format('MMMM Do')}</Typography>
                         </Grid>
                     </Grid>
-                    <Alert variant="outlined" severity="success">
-                        Great news! You have <b>$153.52</b> remaining after all bills.
-                    </Alert>
+                    { handleStatusInfo(upcomingData.money_remaining) }
                 </InfoPaper>
             </Grid>
             <Grid item xs={12} md={6}>
                 <InfoPaper style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
                     <Box>
                         <Typography variant="h6" style={{fontWeight: 'bold'}}><QueryStats /> Your Financial Outlook</Typography>
-                        <br/>
+                        <br />
                         <Line
-                            data={chartData}
+                            data={dynamicChartData}
                             options={{
                                 plugins: {
                                     tooltip: {
